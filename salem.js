b0f7037ee8ed372df7d7f1d7e12fb6058c0aea5d
Nicolas Artman <nicolasartman@gmail.com>
2015-05-09 03:01:00 -0700

    Release version 4, yet another complete product remake!

--- salem.js
+++ salem.js
@@ -1,6 +1,6 @@
-// ========================
-// = WARNING: HACKTACULAR =
-// ========================
+// =============================
+// = WARNING: EPIC HACKTACULAR =
+// =============================
 
 (function () {
   'use strict';
@@ -16,22 +16,37 @@
     .controller('roleStriker', ['$scope', function ($scope) {
 			var isLeftClick = function (clickEvent) {
 				return clickEvent && clickEvent.which === 1;
-			}
+			};
+
 			var isRightClick = function (clickEvent) {
 				return clickEvent && clickEvent.which === 3;
-			}
+			};
+
+			var createPlayer = function (playerNumber) {
+				return {
+					number: playerNumber,
+					// The information being intuited from their in game chatter
+					// One of: null, 'mafiaAlly', 'chatty', 'mafiaesque'
+					talkingSignal: null,
+					// The yay/nay bring to the stand votes (playerNumber => isYayVote)
+					trialVotes: {},
+					// The yay/nay lynch votes (playerNumber => isYayVote)
+					lynchVotes: {}
+				};
+			};
 
 			var initializePlayers = function () {
-				$scope.playerBeingEdited = null;
+				$scope.selectedPlayer = null;
 				$scope.players = _.map(_.range(0, 15), function (number) {
-					return {
-						number: number,
-						isSuspicious: false,
-						votes: {},
-						nominations: {},
-						alignment: null
-					};
+					return createPlayer(number);
 				});
+				// A temporary store for the in-progress voting to put someone
+				// up on the stand.
+				$scope.trialVotes = {};
+			};
+
+			var clearPlayer = function (player) {
+				$scope.players[player.number] = createPlayer(player.number);
 			};
 
       $scope.alignmentMode = false;
@@ -47,62 +62,110 @@
 			// Set the player being edited to the given player,
 			// or exit edit mode if the player being edited has
 			// their edit button clicked again.
-			var editPlayer = function (player) {
-				if (player !== $scope.playerBeingEdited) {
-					$scope.playerBeingEdited = player;
+			var selectPlayer = function (player) {
+				if (player && player !== $scope.selectedPlayer) {
+					$scope.selectedPlayer = player;
 				} else {
-					$scope.playerBeingEdited = null;
+					$scope.selectedPlayer = null;
+				}
+				// Clear any trial votes, since selecting a player
+				// before saving them to the player's record indicates
+				// the vote failed to bring them to the stand
+				$scope.trialVotes = {};
+			};
+
+			this.selectOrClearPlayer = function (clickEvent, player) {
+				// Select player
+				if (isLeftClick(clickEvent)) {
+					selectPlayer(player);
+				}
+				else if (isRightClick(clickEvent)) {
+					clearPlayer(player);
+					selectPlayer(null);
 				}
 			};
 
-			var markPlayerAsDead = function (player) {
-				player.isDead = !player.isDead;
+			var markTrialVote = function (clickEvent, player, isSelectedPlayer) {
+				// Mark that this player has nominated/voted
+				if (!isSelectedPlayer) {
+					// A vote to the stand and the player is the voter
+					if (isLeftClick(clickEvent)) {
+						$scope.trialVotes[player.number] = true;
+					}
+					// Record the abstainers to this player's profile, since they've
+					// now been put up on the stand, then select this player so
+					// trial votes can be viewed and lynching votes can be noted.
+					else if (isRightClick(clickEvent)) {
+						player.trialVotes = $scope.trialVotes;
+						
+						// Clear the lynch votes as we're now about to edit them
+						player.lynchVotes = {};
+
+						selectPlayer(player);
+					}
+				}
 			};
 			
-			var markPlayerAlignment = function (player, alignment) {
-				if (player.alignment !== alignment) {
-					player.alignment = alignment;
+			var markOrClearVote = function (votesMap, playerNumber, voteValue) {
+				if (!votesMap[playerNumber]) {
+					votesMap[playerNumber] = voteValue;
+				} else if (votesMap[playerNumber] === voteValue) {
+					votesMap[playerNumber] = null;
+				}
+			};
+
+			var markLynchVote = function (clickEvent, selectedPlayer, votingPlayer, isSelectedPlayer) {
+				if (!isSelectedPlayer) {
+					// A deliberate abstain and the player is the voter
+					if (isLeftClick(clickEvent)) {
+						markOrClearVote(selectedPlayer.lynchVotes, votingPlayer.number, 'abstain')
+					} else if (isRightClick(clickEvent)) {
+						markOrClearVote(selectedPlayer.lynchVotes, votingPlayer.number, 'lynch')
+					}
 				} else {
-					player.alignment = null;
+					if (isRightClick(clickEvent)) {
+						selectPlayer(null);
+					}
 				}
-			}
+			};
 
-			this.actOnPlayer = function (clickEvent, player) {
-				// Left click
-				if (isLeftClick(clickEvent)) {
-					editPlayer(player);
-				}
-				// Right click
-				else if (isRightClick(clickEvent)) {
-					markPlayerAsDead(player);
-				}
-			}
+			this.markVote = function (clickEvent, player, isAPlayerSelected, players) {
+				var isTheSelectedPlayer =
+						player.number === ($scope.selectedPlayer && $scope.selectedPlayer.number);
 
-			this.markVote = function (clickEvent, votingPlayer, accusedPlayer) {
-				// Players can't vote on themselves, so replace that with
-				// the ability to mark that player as good or bad
-				if (votingPlayer === accusedPlayer) {
-					return markPlayerAlignment(accusedPlayer, 'town');
+				if (!isAPlayerSelected) {
+					markTrialVote(clickEvent, player, isTheSelectedPlayer);
+				} else {
+					markLynchVote(clickEvent, $scope.selectedPlayer, player, isTheSelectedPlayer);
 				}
-				
-				if (isLeftClick(clickEvent)) {
-					votingPlayer.votes[accusedPlayer.number] = votingPlayer.votes[accusedPlayer.number] !== 'guilty' ? 'guilty': null;
-				}
-				else if (isRightClick(clickEvent)) {
-					votingPlayer.votes[accusedPlayer.number] = votingPlayer.votes[accusedPlayer.number] !== 'innocent' ? 'innocent': null;
-				}
-				console.log('voting player votes', votingPlayer.votes);
-			}
-
-			this.markNomination = function (playerBeingNominated, nominator) {
-				// Players can't vote on themselves, so replace that with
-				// the ability to mark that player as good or bad
-				if (playerBeingNominated === nominator) {
-					return markPlayerAlignment(nominator, 'mafia');
-				}
-				playerBeingNominated.nominations[nominator.number] = !playerBeingNominated.nominations[nominator.number];
-			}
+			};
 			
+			this.hasTrialData = function (player) {
+				return player && _.any(player.trialVotes, _.identity);
+			};
+
+			this.markTalkingSignal = function (clickEvent, player) {
+				console.log(clickEvent, player);
+				if (isRightClick(clickEvent)) {
+					if (player.talkingSignal === null) {
+						player.talkingSignal = 'mafiaAlly';
+					} else if (player.talkingSignal === 'mafiaAlly') {
+						player.talkingSignal = null;
+					} else if (player.talkingSignal === 'chatty') {
+						player.talkingSignal = 'mafiaesque';
+					} else {
+						player.talkingSignal = null;
+					}
+				}
+				else if (isLeftClick(clickEvent)) {
+					if (player.talkingSignal === null) {
+						player.talkingSignal = 'chatty';
+					} else {
+						player.talkingSignal = null;
+					}
+				}
+			};
+
 			initializePlayers();
     }])
 
@@ -112,11 +175,10 @@
         '<div class="role-striker-container" ng-controller="roleStriker as playerController" ng-class="{\'alignment-mode\': alignmentMode}">' +
           '<div class="drag-handle" ng-mousedown="toggleAlignmentMode($event)"></div>' +
           '<div class="players">' +
-            '<div class="player" ng-repeat="player in players" ng-class="{\'editable\': playerBeingEdited !== null, \'player-being-edited\': player === playerBeingEdited, \'is-dead\': player.isDead, \'is-suspicious\': player.isSuspicious, \'is-town\': player.alignment === \'town\', \'is-mafia\': player.alignment === \'mafia\'}">' +
-							'<div class="player-control nomination" ng-class="{\'is-nominating\': playerBeingEdited.nominations[player.number]}" ng-click="playerController.markNomination(playerBeingEdited, player)"></div>' +
-							'<div class="player-control vote" ng-class="{\'guilty\': playerBeingEdited.votes[player.number] == \'guilty\', \'innocent\': playerBeingEdited.votes[player.number] == \'innocent\'}" ng-mousedown="playerController.markVote($event, playerBeingEdited, player)"></div>' +
-							'<div class="player-control player-number" ng-mousedown="playerController.actOnPlayer($event, player)"></div>' +
-              '<div class="player-read" ng-click="player.isSuspicious = !player.isSuspicious"></div>' +
+            '<div class="player" ng-repeat="player in players" ng-class="{\'editable\': selectedPlayer !== null, \'has-trial-data\': playerController.hasTrialData(player), \'abstained-from-voting-for-selected-player\': selectedPlayer && playerController.hasTrialData(selectedPlayer) && !selectedPlayer.trialVotes[player.number], \'player-being-edited\': player === selectedPlayer, \'is-dead\': player.isDead, \'is-suspicious\': player.isSuspicious, \'is-town\': player.alignment === \'town\', \'is-mafia\': player.alignment === \'mafia\'}">' +
+							'<div class="player-control player-number" ng-mousedown="playerController.selectOrClearPlayer($event, player)"></div>' +
+              '<div class="player-behavior player-talk" ng-class="{\'is-chatty\': player.talkingSignal === \'chatty\', \'is-mafia-ally\': player.talkingSignal === \'mafiaAlly\', \'is-mafiaesque\': player.talkingSignal === \'mafiaesque\'}" ng-mousedown="playerController.markTalkingSignal($event, player)"></div>' +
+              '<div class="player-behavior player-vote" ng-class="{\'has-voted-up\': trialVotes[player.number], \'has-voted-lynch\': selectedPlayer.lynchVotes[player.number] === \'lynch\', \'has-abstained-from-lynch-vote\': selectedPlayer.lynchVotes[player.number] === \'abstain\'}" ng-mousedown="playerController.markVote($event, player, selectedPlayer, players)"></div>' +
             '</div>' +
           '</div>' +
         '</div>'
@@ -171,8 +233,8 @@
         rel: 'stylesheet',
         type: 'text/css',
         // TODO: CHANGE BEFORE LOCAL DEV!!!!!!!!!!!!!!!!!
-        href: 'http://true-reality.net/cdn/intuition.css'
-        // href: 'http://localhost:8777/intuition.css'
+        href: 'http://true-reality.net/cdn/salem.css'
+        // href: 'http://localhost:8777/salem.css'
       }).appendTo('head');
 
       // Kick it off
